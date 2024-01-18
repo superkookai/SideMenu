165a6dd81333a4cfeb5c963bdd94c44589c1bd0e
Weerawut Chaiyasomboon <weerawut@live.com>
2024-01-18 09:08:42 +0700

    Initial Commit

---
+++ SideMenu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SideMenu
+//
+//  Created by Weerawut Chaiyasomboon on 18/1/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SideMenu/SideMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenuApp.swift
+//  SideMenu
+//
+//  Created by Weerawut Chaiyasomboon on 18/1/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct SideMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
